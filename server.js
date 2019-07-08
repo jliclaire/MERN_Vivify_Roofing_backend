966154743692eebb4c30b9545b4d2606a860f884
jingli <jingli.claire@gmail.com>
2019-07-08 16:55:54 +1000

    install dependencies express, dotenv, axios, jwt, bcrypt, cors, mongoose, nodemon

---
+++ server.js
@@ -0,0 +1,11 @@
+// command for future use
+// require("dotenv").config();
+// const express = require("express");
+// const app = express();
+// const mongoose = require("mongoose");
+// const axios = require("axios");
+// const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcrypt");
+// const cors = require("cors");
+
+// app.use(cors());
