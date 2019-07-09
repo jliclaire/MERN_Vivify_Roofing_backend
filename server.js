529ee77b39f0cdc0e2eafad24b29288496cc835a
jingli <jingli.claire@gmail.com>
2019-07-09 16:17:19 +1000

    backend deploy with now, mongoDB Atlas

--- server.js
+++ server.js
@@ -17,11 +17,21 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 // app.use(cors());
 
-// db authentication
-const mongoPROD_URI = process.env.MONGO_PROD_URI;
+//require User schema
+const User = require("./models/User");
+
+//local db
+// const mongo_URI = process.env.DB_URL;
+// mongoose.connect(mongo_URI, { useNewUrlParser: true }, err => {
+//   if (err) return console.log(`${err}`);
+//   console.log("connected to mongodb");
+// });
 
 //require schema
-const Job = require("./models/Job");
+// const Job = require("./models/Job");
+
+// Cloud db
+const mongoPROD_URI = process.env.DB_URL;
 
 mongoose.connect(mongoPROD_URI, { useNewUrlParser: true }, err => {
   if (err) return console.log(`${err}`);
@@ -32,6 +42,20 @@ app.get("/test", (req, res) => {
   res.send({
     status: "success",
     message: "testing for deployment"
+  });
+});
+
+//below post end to test db, both working on local and cloud
+app.post("/user", async (req, res) => {
+  const { name, password } = req.body;
+  const newUser = new User({
+    name,
+    password
+  });
+
+  const savedUser = await newUser.save();
+  res.send({
+    newUser: savedUser
   });
 });
 
