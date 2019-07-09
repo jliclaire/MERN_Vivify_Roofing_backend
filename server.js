bad7138b2914b0e7ecd777554e3bdc1323984b80
jingli <jingli.claire@gmail.com>
2019-07-09 12:22:36 +1000

    add credential to .env

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 // command for future use
-// require("dotenv").config();
-// const express = require("express");
-// const app = express();
-// const mongoose = require("mongoose");
 // const axios = require("axios");
 // const jwt = require("jsonwebtoken");
 // const bcrypt = require("bcrypt");
 // const cors = require("cors");
 
+//define Port
+const PORT = 5000;
+
+//middleware
+// body-parser configuration
+app.use(express.json());
 // app.use(cors());
 
-//mongoDB Atlas, need to set password in .env
-const mongoPROD_URI =
-  "mongodb+srv://andy:<password>@cluster0-de0ww.mongodb.net/test?retryWrites=true&w=majority";
+// db authentication
+const mongoPROD_URI = process.env.MONGO_PROD_URI;
+
+//require schema
+const Job = require("./models/Job");
 
 mongoose.connect(mongoPROD_URI, { useNewUrlParser: true }, err => {
   if (err) return console.log(`${err}`);
   console.log("connected to mongodb");
+});
+
+app.get("/", (req, res) => {
+  res.send("testing for deployment");
+});
+
+app.listen(PORT, () => {
+  console.log(`listening to port ${PORT}`);
 });
