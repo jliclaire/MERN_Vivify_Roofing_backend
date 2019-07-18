0b74b88b5ed96baad7712e4356bb1b4cb5a1c8d7
Andrew Sims <acsims@tuta.io>
2019-07-19 09:42:48 +1000

    endpoints for users

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,7 @@ router.use(express.json());
 
 router.use("/", require("./authRoutes"));
 router.use("/jobs", require("./jobRoutes"));
+// These need to be auth-protected
+router.use('/users', require('./userRoutes'));
 
 module.exports = router;
