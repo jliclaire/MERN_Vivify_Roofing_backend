11e1c599d356966e632ad3736281bd3058e323db
Andrew Sims <acsims@tuta.io>
2019-07-19 09:49:50 +1000

    Fixed faulty imports

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { login, register, allUsers, deleteUser } = require('../controllers/authController');
+const { login, register } = require('../controllers/authController');
 
-router.get('/users', allUsers);
 router.post('/login', login);
 router.post('/register', register);
-router.delete('/users/:id', deleteUser);
 
 module.exports = router;
