32d5fc3fa65b1f92f35bc3ad0698f048418311be
Andrew Sims <acsims@tuta.io>
2019-07-22 11:39:10 +1000

    Finish auth on backend

--- controllers/authController.js
+++ controllers/authController.js
@@ -31,6 +31,7 @@ const login = async (req, res) => {
 
 const register = async (req, res) => {
   const { name, email, password, role, phone } = req.body;
+  console.log(req.body)
   if (email && password) {
     try {
       const query = await User.findOne({ email: email });
